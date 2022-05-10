eaf53d0e424484200164f2520223c7b2a7df599d
ShiWenber <1210169842@qq.com>
2022-05-10 22:20:06 +0800

    feat(src): add the function "sortrows", "combineStats" to compute the union of two Mats

--- hello_opencv/hello_opencv.cpp
+++ hello_opencv/hello_opencv.cpp
@@ -7,7 +7,104 @@
 using namespace std;
 using namespace cv;
 
+vector<Vec3b> colors;
+void displayImage(Mat, Mat);
+int imageId = 1;
 
+
+Mat removeRow(Mat src, Mat& dst, int row) {
+    if (src.rows == 1) {
+        dst = src;
+        return src;
+    }
+    Mat res1;
+    Mat res2;
+    Mat input = src.clone();
+    input(Range(0, row), Range(0, input.cols)).copyTo(res1);
+    input(Range(row + 1, input.rows), Range(0, input.cols)).copyTo(res2);
+
+    // 合并两块
+    vconcat(res1, res2, res1);
+    dst = res1.clone();
+
+    return res1;
+
+}
+
+
+
+// 合并连通域框
+Mat combineStats(Mat src, Mat& dst, Mat img, float threshold = 0) {
+    Mat input = src.clone();
+    for (int i = 1; i < input.rows; i++) {
+        int x1 = input.at<int>(i, CC_STAT_LEFT);
+        int y1 = input.at<int>(i, CC_STAT_TOP);
+        int w1 = input.at<int>(i, CC_STAT_WIDTH);
+        int h1 = input.at<int>(i, CC_STAT_HEIGHT);
+        int area1 = input.at<int>(i, CC_STAT_AREA);
+
+        for (int j = i + 1; j < input.rows; j++) {
+            int x2 = input.at<int>(j, CC_STAT_LEFT);
+            int y2 = input.at<int>(j, CC_STAT_TOP);
+            int w2 = input.at<int>(j, CC_STAT_WIDTH);
+            int h2 = input.at<int>(j, CC_STAT_HEIGHT);
+            int area2 = input.at<int>(j, CC_STAT_AREA);
+            // 判断1 ， 2两区域是否重叠 计算式原理：见leetcode相关题目，https://blog.csdn.net/qq_43539599/article/details/104952828
+            // int notOverlapped = (x1 + w1 < x2 || x2 + w2 < x1 || y1 + h1 < y2 || y2 + h2 < y1);
+            int overlapped = (x1 < x2 + w2 && x2 < x1 + w1 && y1 < y2 + h2 && y2 < y1 + h1);
+
+            if (overlapped == 0) {
+                continue;
+            }
+
+            // 确定重叠区边界
+            int left = max(x1, x2);
+            int top = max(y1, y2);
+            int right = min(x1 + w1, x2 + w2);
+            int bottom = min(y1 + h1, y2 + h2);
+
+
+            // 计算重叠区域面积
+            int overlap = (right - left) * (bottom - top);
+
+            // 如果重叠区域面积大于阈值，则合并区域
+            if (overlap > threshold) {
+                // 确定合并区域的 x, y, w, h, area
+                int x = min(x1, x2);
+                int y = min(y1, y2);
+                int w = max(x1 + w1, x2 + w2) - x;
+                int h = max(y1 + h1, y2 + h2) - y;
+                int area = w * h;
+                input.at<int>(i, CC_STAT_LEFT) = x;
+                input.at<int>(i, CC_STAT_TOP) = y;
+                input.at<int>(i, CC_STAT_WIDTH) = w;
+                input.at<int>(i, CC_STAT_HEIGHT) = h;
+                input.at<int>(i, CC_STAT_AREA) = area;
+                // 删除被合并的x2区域
+                if (input.rows == 2) {
+                    dst = input.clone();
+                    return input;
+                }
+                removeRow(input, input, j);
+                // 更新区域1
+                x1 = x;
+                y1 = y;
+                w1 = w;
+                h1 = h;
+                area1 = area;
+
+                j--; // 复位
+                displayImage(img, input);
+            }
+
+
+        }
+
+
+    }
+    dst = input.clone();
+    return input;
+}
 
 Mat sortrows(Mat src, Mat& dst, int sort_col) {
     Mat input = src.clone();
@@ -25,18 +122,54 @@ Mat sortrows(Mat src, Mat& dst, int sort_col) {
     return input;
 
 }
+void displayImage(Mat img, Mat stats) {
+    Mat temp_img = img.clone();
+    int number = stats.rows;
+    sortrows(stats, stats, CC_STAT_AREA);
+    cout << stats << endl;
+
+    // 最大连通域是全部图像，因此最大面积或者stats第一行要去除
+    for (int i = 1; i < number; i++)
+    {
+        // 中心位置
+        int center_x = stats.at<int>(i, CC_STAT_LEFT) + stats.at<int>(i, CC_STAT_WIDTH) / 2;
+		int center_y = stats.at<int>(i, CC_STAT_TOP) + stats.at<int>(i, CC_STAT_HEIGHT) / 2;
+            //矩形边框
+        int x = stats.at<int>(i, CC_STAT_LEFT);
+        int y = stats.at<int>(i, CC_STAT_TOP);
+        int w = stats.at<int>(i, CC_STAT_WIDTH);
+        int h = stats.at<int>(i, CC_STAT_HEIGHT);
+        int area = stats.at<int>(i, CC_STAT_AREA);
+
+        // 画框之前先遍历矩阵查找是否存在可以合并的内容
+
+        // 中心位置绘制
+        //circle(temp_img, Point(center_x, center_y), 2, Scalar(0, 255, 0), 2, 8, 0);
+        circle(temp_img, Point(center_x, center_y), 2, colors[i], 2, 8, 0);
+        // 外接矩形
+        Rect rect(x, y, w, h);
+        rectangle(temp_img, rect, colors[i], 1, 8, 0);
+        //putText(temp_img, format("%d", i + 1), Point(center_x, center_y),
+        //    FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
+        putText(temp_img, format("%d", i), Point(center_x, center_y),
+            FONT_HERSHEY_SIMPLEX, 0.5, colors[i], 1);
+
+        cout << "number: " << i << ",area: " << area <<
+            ",x: " << x <<
+            ",y: " << y <<
+            ",w: " << w <<
+            ",h: " << h << endl;
 
 
-Mat combineStats(Mat src, Mat& dst, float threshold = 0) {
-    Mat input = src.clone();
-    for (int i = 0; i < input.rows; i++) {
-        for (int j = i + 1; i < input.rows; i++)
-			
     }
+    //显示结果
+    
+    imshow(to_string(imageId++), temp_img);
 
+    waitKey(0);
 
 }
-	
+
 
 int main()
 {
@@ -60,6 +193,7 @@ int main()
     system("color F0");  //更改输出界面颜色
     //对图像进行距离变换
     Mat img = imread(imagePath + imageFileName);
+    Mat originImage = img.clone();
     if (img.empty())
     {
         cout << "请确认图像文件名称是否正确" << endl;
@@ -78,7 +212,6 @@ int main()
     Mat out, stats, centroids;
     //统计图像中连通域的个数
     int number = connectedComponentsWithStats(leavesBW, out, stats, centroids, 8, CV_16U);
-    vector<Vec3b> colors;
     for (int i = 0; i < number; i++)
     {
         //使用均匀分布的随机数确定颜色
@@ -146,6 +279,12 @@ int main()
     imwrite(imagePath + "proc2.jpg", img);
 
     waitKey(0);
+
+    Mat temp;
+    combineStats(stats, temp,originImage, 0);
+
+	
+
     return 0;
 }
 
